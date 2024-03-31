8b0400498bb9c0690a67f8eff577149d3676168a
Ubaid Qureshi <159818516+parween47@users.noreply.github.com>
2024-03-31 22:21:06 +0500

    Created a command line number guessing game

---
+++ index.ts
@@ -0,0 +1,15 @@
+#! /usr/bin/env node
+
+import inquirer from  'inquirer';
+const realNumber=Math.floor(Math.random()*10+1);
+const answer=await  inquirer.prompt([{
+    type: "number", 
+    message: "Please guess a number between 1-10.",
+    name: "userGuessedNumber",
+}]);
+
+if (answer.userGuessedNumber===realNumber) {
+    console.log("Congratulations! You Guess right number!");
+} else{
+    console.log("You guessed wrong number. The correct one is "+realNumber);
+}
